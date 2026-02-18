8be4eebd4a4f436bd8ef38a051313331d3191c44
rcbassie <rcbass27@gmail.com>
2026-02-18 12:52:43 -0600

    hopper

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,19 +1,23 @@
 package frc.robot.commands;
 
+import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class IntakeCommand extends Command {
     IntakeSubsystem intake;
+    HopperSubsystem hopper;
 
-    public IntakeCommand(IntakeSubsystem intake) {
+    public IntakeCommand(IntakeSubsystem intake, HopperSubsystem hopperSubsystem) {
         this.intake = intake;
+        this.hopper = hopper;
     }
 
     @Override
     public void initialize() {
         intake.indexIn();
         intake.intake();
+        hopper.conveyorIn();
 
     }
 
@@ -21,6 +25,7 @@ public class IntakeCommand extends Command {
     public void end(boolean interrupted) {
         intake.stopIndex();
         intake.stopShooter();
+        hopper.conveyorStop();
     }
 
 }
--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,20 +1,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class ShootCommand extends Command {
 
     IntakeSubsystem intake;
+    HopperSubsystem hopper;
 
-    public ShootCommand(IntakeSubsystem intake) {
+    public ShootCommand(IntakeSubsystem intake, HopperSubsystem hopper) {
         this.intake = intake;
+        this.hopper = hopper;
     }
 
     @Override
     public void initialize() {
         intake.indexOut();
         intake.shoot();
+        hopper.conveyorOut();
 
     }
 
@@ -22,6 +26,7 @@ public class ShootCommand extends Command {
     public void end(boolean interrupted) {
         intake.stopIndex();
         intake.stopShooter();
+        hopper.conveyorStop();
     }
 
 }
